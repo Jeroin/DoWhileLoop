15cf34d2a7662e3b47827317f1b55c3b132d860a
Jeroin <cjeroin@gmail.com>
2020-10-15 10:04:51 +0800

    initial commit

---
+++ src/Looping.java
@@ -0,0 +1,14 @@
+
+public class Looping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int age = 5;
+		
+		do {
+			System.out.println("Minor age " + age);
+			age++;
+		}while(age < 18);
+	}
+
+}
